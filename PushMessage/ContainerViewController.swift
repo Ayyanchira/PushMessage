2d9c2b9ef67e7de5ec210f1ef3e361a2b64459b7
Akshay <ayyanchira.akshay@gmail.com>
2017-11-16 13:40:33 -0500

    Basic implementation of side drawable side panel

---
+++ PushMessage/ContainerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ContainerViewController.swift
+//  PushMessage
+//
+//  Created by Ayyanchira, Akshay Murari on 11/16/17.
+//  Copyright © 2017 Ayyanchira, Akshay Murari. All rights reserved.
+//
+
+import UIKit
+import SidebarOverlay
+
+class ContainerViewController: SOContainerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.menuSide = .left
+        self.topViewController = self.storyboard?.instantiateViewController(withIdentifier: "MessageView")
+        self.sideViewController = self.storyboard?.instantiateViewController(withIdentifier: "SidePanel")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
